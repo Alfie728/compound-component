972ee2273c3183765f29aae0c4197b4a02976bb7
Alfie728 <zegro728@gmail.com>
2024-12-23 15:23:20 +1100

    feat: added Radix Accordion example

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ToggleDemo } from "./components/demos/ToggleDemo";
 import { AccordionDemo } from "./components/demos/AccordionDemo";
-
+import AccordionDemo2 from "./components/demos/RadixAccordionDemo";
 function App() {
   return (
-    <>
+    <main className="bg-slate-300 flex flex-col items-center justify-center h-screen">
       {/* <ToggleDemo /> */}
-      <AccordionDemo />
-    </>
+      {/* <AccordionDemo /> */}
+      <AccordionDemo2 />
+    </main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+import { mauve, violet, blackA, whiteA } from "@radix-ui/colors";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...mauve,
+        ...violet,
+        ...blackA,
+        ...whiteA,
+      },
+      keyframes: {
+        slideDown: {
+          from: { height: "0px" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        slideUp: {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0px" },
+        },
+      },
+      animation: {
+        slideDown: "slideDown 300ms cubic-bezier(0.87, 0, 0.13, 1)",
+        slideUp: "slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1)",
+      },
+    },
   },
   plugins: [],
 };
